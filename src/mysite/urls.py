fcf9ca42c25de0aa5f18c564dee25e86f38193fa
Amit Kumar <amitkumar@Amits-MacBook-Pro.local>
2020-07-12 12:13:09 +0530

    create blog posts

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -12,6 +12,7 @@ from account.views import (
     logout_view,
     login_view,
     account_view,
+    must_authenticate_view,
 )
 
 urlpatterns = [
@@ -22,6 +23,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('account/', account_view, name='account'),
     path('blog/', include('blog.urls'), name='blog'),
+    path('must_authenticate/', must_authenticate_view, name='must_authenticate'),
 
     # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
